e0d60392302deb7d21cac0b7e9e12d953ed75345
L1mus <ali.firefox@yahoo.com>
2026-06-06 22:08:29 +0700

    feat: add enum for type_seat

--- db/migrations/000010_create_seats_table.up.sql
+++ db/migrations/000010_create_seats_table.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE seats (
-                       cinema_id INTEGER,
-                       seat_number INTEGER NOT NULL,
-                       row VARCHAR(10) NOT NULL,
-                       CONSTRAINT fk_seats_cinema FOREIGN KEY (cinema_id) REFERENCES cinemas(id)
+    id SERIAL PRIMARY KEY,
+    cinema_id INTEGER,
+    seat_number INTEGER NOT NULL,
+    row VARCHAR(10) NOT NULL,
+    seat_type type_seat DEFAULT 'regular',
+    CONSTRAINT fk_seats_cinema FOREIGN KEY (cinema_id) REFERENCES cinemas(id)
 );
