2a4fc8f43de6e04d40413cf13f6853dfc946a436
L1mus <ali.firefox@yahoo.com>
2026-06-07 21:16:43 +0700

    feat: add method GetUserProfile

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/L1mus/Tickitz-backend/internal/model"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -12,4 +15,23 @@ func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{
 		db: db,
 	}
+}
+
+func (r *UserRepository) GetUserProfile(ctx context.Context, userID int) (*model.UserProfile, error) {
+	const q = `
+		SELECT
+			COALESCE(first_name, '') AS first_name,
+			COALESCE(last_name, '') AS last_name,
+			email,
+			COALESCE(phone, '') AS phone
+		FROM users
+		WHERE id = $1`
+
+	row := r.db.QueryRow(ctx, q, userID)
+	var up model.UserProfile
+	err := row.Scan(&up.FirstName, &up.LastName, &up.Email, &up.Phone)
+	if err != nil {
+		return nil, err
+	}
+	return &up, nil
 }
