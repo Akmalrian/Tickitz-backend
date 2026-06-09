b20f5f29ec1d742c069c8d939edd30dd701f12c9
l1mus <ali.firefox@yahoo.co.id>
2026-06-09 19:43:24 +0700

    feat: add service Logic for feature GetSeats

--- internal/service/order.service.go
+++ internal/service/order.service.go
@@ -7,6 +7,7 @@ import (
 	apperror "github.com/L1mus/Tickitz-backend/internal/appError"
 	"github.com/L1mus/Tickitz-backend/internal/dto"
 	"github.com/L1mus/Tickitz-backend/internal/repository"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -53,7 +54,38 @@ func (s *OrderService) GetSeats(ctx context.Context, showtimeID int) (dto.SeatPa
 		return dto.SeatPageResponse{}, err
 	}
 
-func (s *OrderService) CreateBooking() {
+	var seats []dto.SeatDTO
+	for _, s := range data {
+		seat := dto.SeatDTO{
+			SeatID:     s.SeatID,
+			Row:        s.Row,
+			SeatNumber: s.SeatNumber,
+			SeatType:   s.SeatType,
+			SeatStatus: s.SeatStatus,
+		}
+		seats = append(seats, seat)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return dto.SeatPageResponse{}, err
+	}
+
+	return dto.SeatPageResponse{
+		Summary: dto.SummaryMovieDTO{
+			MovieTitle:  detailMovie.MovieTitle,
+			MoviePoster: detailMovie.MoviePoster,
+			Category:    detailMovie.Category,
+			CinemaName:  detailMovie.CinemaName,
+			CinemaLogo:  detailMovie.CinemaLogo,
+			ShowDate:    detailMovie.ShowDate,
+			ShowTime:    detailMovie.ShowTime,
+			TicketPrice: detailMovie.TicketPrice,
+		},
+		Seats: seats,
+	}, nil
+}
+
+func (s *OrderService) CreateBooking(ctx context.Context, req dto.CreateBookingRequest, userID int) (dto.CreateBookingResponse, error) {
 	/*
 	   Validasi: pastikan req.SeatIDs tidak kosong
 	   Validasi: pastikan jumlah seat == req.Quantity
@@ -71,4 +103,56 @@ func (s *OrderService) CreateBooking() {
 	   Commit DB transaction
 	   return booking_id, nil
 	*/
+	if len(req.SeatIDs) == 0 || len(req.SeatIDs) < 0 {
+		return dto.CreateBookingResponse{}, apperror.InvalidSeatsInput
+	}
+	if len(req.SeatIDs) != req.Quantity {
+		return dto.CreateBookingResponse{}, apperror.InvalidQuantity
+	}
+	tx, err := s.db.Begin(ctx)
+	if err != nil {
+		log.Println("failed to begin transaction", err)
+	}
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			log.Println("rollback transaction: ", err.Error())
+		}
+	}(tx, ctx)
+	detailMovie, err := s.orderRepo.GetShowtimeSummary(ctx, tx, req.ShowtimeID)
+	if err != nil {
+		return dto.CreateBookingResponse{}, err
+	}
+
+	dataSeats, err := s.orderRepo.GetSeatsByShowtime(ctx, tx, req.ShowtimeID, detailMovie.CinemaID)
+	if err != nil {
+		return dto.CreateBookingResponse{}, err
+	}
+
+	for _, s := range dataSeats {
+		for _, ts := range req.SeatIDs {
+			if s.SeatID == ts && s.SeatStatus != "Available" {
+				return dto.CreateBookingResponse{}, apperror.SeatsUnavailable
+			}
+		}
+	}
+
+	bookingId, err := s.orderRepo.CreateBooking(ctx, tx, req, userID)
+	if err != nil {
+		return dto.CreateBookingResponse{}, err
+	}
+
+	for _, v := range req.SeatIDs {
+		if err := s.orderRepo.CreateBookingSeat(ctx, tx, bookingId, v, req.ShowtimeID); err != nil {
+			return dto.CreateBookingResponse{}, err
+		}
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return dto.CreateBookingResponse{}, err
+	}
+
+	return dto.CreateBookingResponse{
+		BookingID: bookingId,
+	}, nil
 }
