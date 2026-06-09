7d02b79f561ac8d8e4ee61dfe18895ba2c13b4a0
ilhammursidi <imursidi37@gmail.com>
2026-06-09 23:39:37 +0700

    feat: add MovieRouter

--- internal/router/movie.route.go
+++ internal/router/movie.route.go
@@ -1,12 +1,17 @@
 package router
 
 import (
+	"github.com/L1mus/Tickitz-backend/internal/controller"
+	"github.com/L1mus/Tickitz-backend/internal/repository"
+	"github.com/L1mus/Tickitz-backend/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/redis/go-redis/v9"
 )
 
-func MovieRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
-	// movieRouter := router.Group("/movies")
-
+func MovieRouter(router *gin.RouterGroup, db *pgxpool.Pool) {
+	movieRouter := router.Group("/movies")
+	movieRepository := repository.NewMovieRepository(db)
+	movieService := service.NewMovieService(movieRepository)
+	movieController := controller.NewMovieController(movieService)
+	movieRouter.GET("", movieController.GetAllMovies)
 }
